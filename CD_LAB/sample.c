16f59f459be4af596f927ff55d3b1f2bfe12cebe
Devadathan N R <devadathannr@gmail.com>
2026-01-27 10:35:53 +0530

    Add files via upload

---
+++ CD_LAB/sample.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/* global variables */
+int a, b, c;
+float price;
+char grade;
+
+int a;
+
+/* function 1 */
+int sum(int x, int y)
+{
+    int result;
+    result = x + y;
+    return result;
+}
+
+/* function 2 */
+double average(double total, int count)
+{
+    double avg;
+    avg = total / count;
+    return avg;
+}
+
+/* function 3 */
+void printMsg()
+{
+    char msg;
+}
+
+int inside; 
+
